f53488f696c24a4ebbf0f45b06cc39eee0f68bda
omer-kuluc <omerkuluc96@gmail.com>
2025-01-20 13:27:29 +0300

    yorum eklenebilir hale geldi

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,11 @@ function App() {
     getComments();
   }, [])
 
-  function addComment(comment) {
-    setComments([...comments, comment]);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    const formData = new FormData(e.target);
-    const formObj = Object.fromEntries(formData);
-    formObj.id = crypto.randomUUID();
-
-    addComment(formObj);
-    e.target.reset();
-  }
-
   return (
     <>
       <div className="container">
         <h2>Comment Listesi</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="comment" placeholder="Yapılacak Yorum" autoComplete="off" />
-          <button type="submit">Comment Ekle</button>
-        </form>
+        <AddComment setComments = {setComments}/>
         {comments.length === 0 ?
           <p>No comments found.</p> :
           <ul>
@@ -62,6 +44,39 @@ function App() {
      </div>
   </>
  )
+}
+
+function AddComment({setComments}) {
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const formObj = Object.fromEntries(formData);
+
+    const newComment = {
+      id : crypto.randomUUID(),
+      name : 'Ömer',
+      time : 'now',
+      comment : formObj.comment,
+      likes : 0,
+      dislikes : 0,
+      replies : []
+    };
+
+    setComments((comments) => [newComment, ...comments]);
+    e.target.reset();
+
+  }
+  return(
+    <>
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="comment" placeholder="Yapılacak Yorum" autoComplete="off" />
+      <button type="submit">Comment Ekle</button>
+    </form>
+    </>
+  )
 }
 
 export default App;
