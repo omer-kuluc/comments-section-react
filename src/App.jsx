cd87b6c0139e17b570050f36362f9a6a4f4bb0f7
omer-kuluc <omerkuluc96@gmail.com>
2025-01-20 12:45:38 +0300

    proje güncellendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    async function getComments() {
+      const data = await fetch("/data/data.json").then(x => x.json());
+      setComments(data)
+    }
+    getComments();
+  }, [])
+
+  function addComment(comment) {
+    setComments([...comments, comment]);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const formObj = Object.fromEntries(formData);
+    formObj.id = crypto.randomUUID();
+
+    addComment(formObj);
+    e.target.reset();
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="container">
+        <h2>Comment Listesi</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="comment" placeholder="Yapılacak Yorum" autoComplete="off" />
+          <button type="submit">Comment Ekle</button>
+        </form>
+        {comments.length === 0 ?
+          <p>No comments found.</p> :
+          <ul>
+            {comments.map((x, i) => <li key={i}>
+              <h2>{x.name}</h2>
+              <span>{x.time}</span>
+              <p>{x.comment}</p>
+              <span>Likes: {x.likes}</span>
+              <span>Dislikes: {x.dislikes}</span>
+              {x.replies && x.replies.length > 0 && (
+                <ul>
+                  {x.replies.map((reply, i) => (
+                    <li key={i}>
+                      <h3>{reply.name}</h3>
+                      <span>{reply.time}</span>
+                      <p>{reply.comment}</p>
+                      <span>Likes: {reply.likes}</span>
+                      <span>Dislikes: {reply.dislikes}</span>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>)}
+          </ul>}
+     </div>
+  </>
+ )
 }
 
-export default App
+export default App;
